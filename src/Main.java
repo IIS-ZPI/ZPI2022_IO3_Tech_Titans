853e16750567bb8cf5c5581baaf8c43b61314d85
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2023-03-02 15:28:59 +0100

    Add printing group name and leader info

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      System.out.println("Group: Tech Titans" + "\nLeader: \n\tid: kasia251011\n\trole: Developer");
     }
 }
