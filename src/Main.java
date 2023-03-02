7f86f988fec2e7a563be2e22e295a37fac163492
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-03-02 15:37:24 +0100

    Added my display information

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
       System.out.println("Group: Tech Titans" + "\nLeader: \n\tid: kasia251011\n\trole: Developer");
+      System.out.println("\nEwangelina\n\tGithub ID: 103572361\n\trole: Developer");
     }
 }
