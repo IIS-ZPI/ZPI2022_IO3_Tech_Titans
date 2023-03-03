f42148054594fad2587e3bd39f8979609c63e8fe
SzymonB5 <235831@edu.p.lodz.pl>
2023-03-03 09:36:40 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
       System.out.println("Group: Tech Titans" + "\nLeader: \n\tid: kasia251011\n\trole: Developer");
+	  System.out.println("\nSzymon:\n\tGitHub id: 99877119\n\trole: Tester");
     }
 }
