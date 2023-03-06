59e472e83fcd7bb9a3a107c0262abde99fa3da60
Mateusz Blaszczynski <blaszczu3@gmail.com>
2023-03-06 15:20:30 +0100

    Display information: Mateusz Blaszczynski

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
       System.out.println("Group: Tech Titans" + "\nLeader: \n\tid: kasia251011\n\trole: Developer");
+      System.out.println("\nMateusz:\n\tgithub id: genoss1\n\trole: Operator");
     }
 }
