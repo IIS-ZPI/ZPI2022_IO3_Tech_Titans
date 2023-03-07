2eb78834f083c6058fbbc426fd1c7891ca20c59a
SzymonB5 <235831@edu.p.lodz.pl>
2023-03-07 20:44:54 +0100

    Displaying information: Szymon Bigoszewski

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
       System.out.println("Group: Tech Titans" + "\nLeader: \n\tid: kasia251011\n\trole: Developer");
+      System.out.println("\nSzymon\n\tgithub id: 121040955\n\trole: Tester");
     }
 }
